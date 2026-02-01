6a0186feec681b56b056bd5deff6bfb64a741199
nojhanac <141612269+nojhanac@users.noreply.github.com>
2026-02-01 16:49:57 +0330

    Update traffic.go

--- xray/traffic.go
+++ xray/traffic.go
@@ -1,9 +1,14 @@
 package xray
 
+import "time"
+
 type Traffic struct {
-	IsInbound  bool
-	IsOutbound bool
-	Tag        string
-	Up         int64
-	Down       int64
+    IsInbound  bool      
+    IsOutbound bool      
+    Tag        string    
+    UserID     int       
+    Up         int64     
+    Down       int64    
+    
+   Timestamp  time.Time 
 }
